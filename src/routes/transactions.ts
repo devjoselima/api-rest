52c4160c82718f53655941e6b97ee0e7ec6f748b
Jose Lima <jalimag0206@gmail.com>
2023-11-26 21:26:28 -0300

    feat: add route to get all transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,12 @@ import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 
 export async function transactionRoutes(app: FastifyInstance) {
+    app.get('/', async () => {
+        const transactions = await knex('transactions').select()
+
+        return { transactions }
+    })
+
     app.post('/', async (request, reply) => {
         const createTransactionBodySchema = z.object({
             title: z.string(),
