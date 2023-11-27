c5ad750a489be40df5c388b7a1d061ae8f5f51f6
Jose Lima <jalimag0206@gmail.com>
2023-11-26 21:25:45 -0300

    feat: add route to create new transaction

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
 import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.register(transactionRoutes)
+app.register(transactionRoutes, {
+    prefix: 'transactions',
+})
 
 app.listen({ port: env.PORT }).then(() => {
     console.log('HTTP Server Running!')
