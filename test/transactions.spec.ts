34d75c0548aeb9d60d756ad5a45763a15f3bd1eb
Jose Lima <jalimag0206@gmail.com>
2023-11-27 19:39:07 -0300

    feat:  add test for listing all transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -12,7 +12,7 @@ describe('Transactions routes', () => {
         await app.close()
     })
 
-    it('should be able to create a new trasanction', async () => {
+    it('should be able to create a new trasaction', async () => {
         await request(app.server)
             .post('/transactions')
             .send({
@@ -21,5 +21,29 @@ describe('Transactions routes', () => {
                 type: 'credit',
             })
             .expect(201)
+    })
+
+    it('should be able to list all transactions', async () => {
+        const createTransactionResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 5000,
+                type: 'credit',
+            })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransactionsResponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200)
+
+        expect(listTransactionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'New transaction',
+                amount: 5000,
+            }),
+        ])
     })
 })
